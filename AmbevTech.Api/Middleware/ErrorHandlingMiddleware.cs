a6f082fb15c0a14771278a7c718095cddd04ead8
Gustavo Antunes Barroso <esx14@localiza.com>
2024-09-25 12:32:59 -0300

    Adicionando logs na classe VendaService Adicionando metodo para logar erros do tipo BusinessException no middleware ErrorHandlingMiddleware

--- AmbevTech.Api/Middleware/ErrorHandlingMiddleware.cs
+++ AmbevTech.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using AmbevTech.Domain.Exception;
+using Microsoft.AspNetCore.Diagnostics;
 using Serilog;
 using System.Net;
 using System.Text.Json;
@@ -10,6 +11,19 @@ namespace AmbevTech.Api.Middleware
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
             Log.Error(exception, "Error");
+
+            string result = JsonSerializer.Serialize(new { error = exception?.Message });
+
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await httpContext.Response.WriteAsync(result);
+
+            return true;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, BusinessException exception, CancellationToken cancellationToken)
+        {
+            Log.Error(exception.Message);
 
             string result = JsonSerializer.Serialize(new { error = exception?.Message });
 
--- AmbevTech.Application/Services/VendaService.cs
+++ AmbevTech.Application/Services/VendaService.cs
@@ -3,6 +3,7 @@ using AmbevTech.Domain.Events;
 using AmbevTech.Domain.Exception;
 using AmbevTech.Domain.Interfaces;
 using AmbevTech.Domain.Models;
+using Serilog;
 
 namespace AmbevTech.Application.Services
 {
@@ -19,6 +20,8 @@ namespace AmbevTech.Application.Services
 
         public async Task<Venda> CreateVendaAsync(Venda venda)
         {
+            Log.Warning($"Venda {venda.NumeroVenda} entrou no metodo {nameof(CreateVendaAsync)}");
+
             Venda vendaDb = await _vendaRepository.GetByIdAsync(venda.NumeroVenda);
             if (vendaDb is not null)
                 throw new BusinessException($"Venda {venda.NumeroVenda} já existente!");
@@ -26,28 +29,38 @@ namespace AmbevTech.Application.Services
             await _vendaRepository.AddAsync(venda);
             await _eventBus.PublishAsync(new CompraCriada(venda));
 
+            Log.Warning($"Venda {venda.NumeroVenda} criada!");
             return venda;
         }
 
         public async Task<Venda> UpdateVendaAsync(Venda venda)
         {
+            Log.Warning($"Venda {venda.NumeroVenda} entrou no metodo {nameof(UpdateVendaAsync)}");
+
             var vendaDb = await ValidarVendaExistente(venda.NumeroVenda);
             await _vendaRepository.UpdateAsync(venda);
             await _eventBus.PublishAsync(new CompraAlterada(venda));
 
+            Log.Warning($"Venda {venda.NumeroVenda} alterada!");
             return venda;
         }
 
         public async Task CancelVendaAsync(int numeroVenda)
         {
+            Log.Warning($"Venda {numeroVenda} entrou no metodo {nameof(CancelVendaAsync)}");
+
             var venda = await ValidarVendaExistente(numeroVenda);
             venda.Cancelado = true;
             await _vendaRepository.UpdateAsync(venda);
             await _eventBus.PublishAsync(new CompraCancelada(numeroVenda));
+
+            Log.Warning($"Venda {venda.NumeroVenda} cancelada!");
         }
 
         public async Task CancelItemAsync(int numeroVenda, int itemId)
         {
+            Log.Warning($"Venda {numeroVenda} Pedido {itemId} entrou no metodo {nameof(CancelItemAsync)}!");
+
             var venda = await ValidarVendaExistente(numeroVenda);
             bool itemExiste = venda.Itens.Any(i => i.Id == itemId);
 
@@ -57,6 +70,8 @@ namespace AmbevTech.Application.Services
             venda.Itens.FirstOrDefault(x => x.Id == itemId).Cancelado = true;
             await _vendaRepository.UpdateAsync(venda);
             await _eventBus.PublishAsync(new ItemCancelado(itemId));
+
+            Log.Warning($"Venda {numeroVenda} Pedido {itemId} cancelado!");
         }
 
         private async Task<Venda> ValidarVendaExistente(int vendaId)
